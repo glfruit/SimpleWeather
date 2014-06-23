58a5cc0db65832e0737819c5f5bd695bbbb805b9
Fruit Lee <glfruit80@gmail.com>
2014-06-23 20:10:02 +0800

    Initial Commit

---
+++ SimpleWeather/HYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Fruit Lee on 14-6-23.
+//  Copyright (c) 2014年 Fruit Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
